6f1c66dc7d21988d8790e2f0ab4aa030cf1b1d28
satiriorn <satiriorn@gmail.com>
2019-03-20 14:27:49 +0200

    day2

--- Source/FallAndContinue/Thorn.cpp
+++ Source/FallAndContinue/Thorn.cpp
@@ -2,12 +2,12 @@
 #include "Kismet/GameplayStatics.h"
 #include "Components/SphereComponent.h"
 
-AThorn::AThorn()
+AThorn::AThorn(const FObjectInitializer& ObjectInitializer):Super(ObjectInitializer)
 {
 	PrimaryActorTick.bCanEverTick = true;
-	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	USphereComponent* SphereComponent = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, TEXT("RootComponent"));
+	Mesh = ObjectInitializer.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("Mesh"));
 	RootComponent = SphereComponent;
-	Mesh = CreateDefaultSubobject < UStaticMeshComponent>(TEXT("FriendlyName"));
 	SphereComponent->InitSphereRadius(25.0f);
 	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));
 	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AThorn::OnOverlap);
@@ -16,7 +16,6 @@ AThorn::AThorn()
 void AThorn::BeginPlay()
 {
 	Super::BeginPlay();
-
 }
 
 void AThorn::OnOverlap(class UPrimitiveComponent* newComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
--- Source/FallAndContinue/Thorn.h
+++ Source/FallAndContinue/Thorn.h
@@ -13,7 +13,7 @@ class FALLANDCONTINUE_API AThorn : public AActor
 	GENERATED_BODY()
 	
 public:	
-	AThorn();
+	AThorn(const FObjectInitializer& ObjectInitializer);
 	UFUNCTION()
 		void OnOverlap(class UPrimitiveComponent* newComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 protected:
