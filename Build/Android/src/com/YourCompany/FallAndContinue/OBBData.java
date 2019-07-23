0597e823008bdeb9f228ab02120ffcad66d321c2
satiriorn <satiriorn@gmail.com>
2019-07-23 19:34:18 +0300

    v0.1m

--- Build/Android/src/com/YourCompany/FallAndContinue/OBBData.java
+++ Build/Android/src/com/YourCompany/FallAndContinue/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-1055193610L // the length of the file in bytes
+551203991L // the length of the file in bytes
 )
 };
 };
